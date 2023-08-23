0f6c43d2bffff1ca1dc9853362280b20f96b19d0
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-08-23 21:13:09 +0200

    Code refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.moodtrackr"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.moodtrackr"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -47,18 +47,20 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.compose.ui:ui:1.0.4")
-    implementation("androidx.compose.ui:ui-graphics:1.0.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.0.4")
-    implementation("androidx.compose.material3:material3:1.0.0")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.ui:ui-graphics:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.0.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.0.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
 }
--- app/src/main/java/com/example/moodtrackr/MainActivity.kt
+++ app/src/main/java/com/example/moodtrackr/MainActivity.kt
@@ -3,28 +3,16 @@ package com.example.moodtrackr
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
 import com.example.moodtrackr.ui.theme.MoodTrackrTheme
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
+// MainActivity.kt
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,144 +25,22 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MoodTrackrApp() {
-    val context = LocalContext.current
-
-    val sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-    val savedName = sharedPreferences.getString("name", "")
-    val savedSurname = sharedPreferences.getString("surname", "")
-    val savedSex = sharedPreferences.getString("sex", "")
-    val savedBirthday = sharedPreferences.getString("birthday", "")
-
-    // Wrap the content of the app with the theme
     MoodTrackrTheme {
-        Content(savedName, savedSurname, savedSex, savedBirthday, sharedPreferences)
+        Content()
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Content(
-    savedName: String?,
-    savedSurname: String?,
-    savedSex: String?,
-    savedBirthday: String?,
-    sharedPreferences: SharedPreferences
-) {
-    var nameState by remember { mutableStateOf(savedName ?: "Name") }
-    var surnameState by remember { mutableStateOf(savedSurname ?: "Surname") }
-    var sexState by remember { mutableStateOf(savedSex ?: "M") }
-    var birthdayState by remember { mutableStateOf(savedBirthday ?: "") }
+fun Content() {
+    val navController = rememberNavController()
 
-    if (birthdayState == null || birthdayState.isBlank())
-        birthdayState = "01/01/1990"
-
-    val selectedDate = LocalDate.parse(birthdayState, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-    val datePicker = rememberDatePickerState(selectedDate.toEpochDay() * (24 * 60 * 60 * 1000))
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        // Description Text
-        Text(
-            text = "Please enter your details:",
-            style = MaterialTheme.typography.titleSmall.copy(
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp,
-                textAlign = TextAlign.Center
-            ),
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
-
-        Text("Name")
-        BasicTextField(
-            value = nameState,
-            onValueChange = { newValue: String -> nameState = newValue },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .padding(8.dp),
-            textStyle = MaterialTheme.typography.bodyMedium,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    // Handle keyboard Done action if needed
-                }
-            ),
-            singleLine = true
-        )
-
-        Text("Surname")
-        BasicTextField(
-            value = surnameState,
-            onValueChange = { newValue: String -> surnameState = newValue },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .padding(8.dp),
-            textStyle = MaterialTheme.typography.bodyMedium,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    // Handle keyboard Done action if needed
-                }
-            ),
-            singleLine = true
-        )
-
-        Text("Sex")
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            RadioButton(
-                selected = sexState == "M",
-                onClick = { sexState = "M" }
-            )
-            Text("M", modifier = Modifier.alignByBaseline())
-            RadioButton(
-                selected = sexState == "F",
-                onClick = { sexState = "F" }
-            )
-            Text("F", modifier = Modifier.alignByBaseline())
+    NavHost(navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController)
         }
-
-        Text("Birthday")
-        DatePicker(
-            state = datePicker,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .padding(8.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                // Save the text field values to SharedPreferences
-                val editor = sharedPreferences.edit()
-                editor.putString("name", nameState)
-                editor.putString("surname", surnameState)
-                editor.putString("sex", sexState)
-                editor.putString("birthday", birthdayState)
-                editor.apply()
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(48.dp)
-        ) {
-            Text("Save")
+        composable("editProfile") {
+            EditProfileScreen(navController)
         }
     }
 }
