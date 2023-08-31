45ce1b73969a36b983fbf59398f70b8d773db1db
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-08-31 21:25:04 +0200

    updated app version #53

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.moodtrackr"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
