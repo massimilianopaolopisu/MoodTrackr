ff0b58c831cbd404b27483348452dadce4703a0b
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-09-02 18:53:37 +0200

    Fixed dagger-compiler version #62

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.1")
     implementation("androidx.navigation:navigation-compose:2.7.1")
     implementation("com.google.dagger:dagger:2.28.3")
-    annotationProcessor("com.google.dagger:dagger-compiler:2.x")
+    annotationProcessor("com.google.dagger:dagger-compiler:2.28.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
