159f1eb3a82ecbbd95b57ff79b10a12c0ac41f89
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-09-04 18:09:01 +0200

    Added new properties in MoodEntry.kt #70

--- app/src/main/java/com/example/moodtrackr/models/MoodEntry.kt
+++ app/src/main/java/com/example/moodtrackr/models/MoodEntry.kt
@@ -2,10 +2,90 @@ package com.example.moodtrackr.models
 
 import java.time.LocalDate
 
-class MoodEntry(var date: LocalDate, var happiness: Int, var love: Int,
-    var energy: Int, var health: Int, var notes: String) {
+class MoodEntry(
+    private var date: LocalDate,
+    private var happiness: Int?,
+    private var anger: Int?,
+    private var love: Int?,
+    private var stress: Int?,
+    private var energy: Int?,
+    private var sleep: Int?,
+    private var health: Int?,
+    private var depression: Int?,
+    private var notes: String?
+) {
+
+    var Date: LocalDate
+        get() = date
+        set(value) {
+            date = value
+        }
+
+    var Happiness: Int
+        get() = clampValue(happiness)
+        set(value) {
+            happiness = clampValue(value)
+        }
+
+    var Anger: Int
+        get() = clampValue(anger)
+        set(value) {
+            anger = clampValue(value)
+        }
+
+    var Love: Int
+        get() = clampValue(love)
+        set(value) {
+            love = clampValue(value)
+        }
+
+    var Stress: Int
+        get() = clampValue(stress)
+        set(value) {
+            stress = clampValue(value)
+        }
+
+    var Energy: Int
+        get() = clampValue(energy)
+        set(value) {
+            energy = clampValue(value)
+        }
+
+    var Sleep: Int
+        get() = clampValue(sleep)
+        set(value) {
+            sleep = clampValue(value)
+        }
+
+    var Health: Int
+        get() = clampValue(health)
+        set(value) {
+            health = clampValue(value)
+        }
+
+    var Depression: Int
+        get() = clampValue(depression)
+        set(value) {
+            depression = clampValue(value)
+        }
+
+    var Notes: String?
+        get() = notes
+        set(value) {
+            notes = value ?: ""
+        }
 
     fun getAverage(): Int {
-        return (happiness + love + energy + health) / 4
+        val values = listOf(Happiness, Anger, Love, Stress, Energy, Sleep, Health, Depression)
+        return values.sum() / values.size
+    }
+
+    private fun clampValue(value: Int?): Int {
+        return when {
+            value == null -> 50
+            value < 1 -> 1
+            value > 100 -> 100
+            else -> value
+        }
     }
 }
--- app/src/main/java/com/example/moodtrackr/repositories/MoodEntriesRepository.kt
+++ app/src/main/java/com/example/moodtrackr/repositories/MoodEntriesRepository.kt
@@ -15,16 +15,20 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
     private val _sqlDatabaseHelper = SqlDatabaseHelper(context)
 
     override fun insertMoodEntry(moodEntry: MoodEntry) {
-        try{
+        try {
             val db = _sqlDatabaseHelper.writableDatabase
 
             val values = ContentValues().apply {
-                put(MoodEntryContract.MoodEntry.COLUMN_DATE, DateUtilities.getStringDateFromLocalDate(moodEntry.date))
-                put(MoodEntryContract.MoodEntry.COLUMN_HAPPINESS, moodEntry.happiness)
-                put(MoodEntryContract.MoodEntry.COLUMN_LOVE, moodEntry.love)
-                put(MoodEntryContract.MoodEntry.COLUMN_ENERGY, moodEntry.energy)
-                put(MoodEntryContract.MoodEntry.COLUMN_HEALTH, moodEntry.health)
-                put(MoodEntryContract.MoodEntry.COLUMN_NOTES, moodEntry.notes)
+                put(MoodEntryContract.MoodEntry.COLUMN_DATE, DateUtilities.getStringDateFromLocalDate(moodEntry.Date))
+                put(MoodEntryContract.MoodEntry.COLUMN_HAPPINESS, moodEntry.Happiness)
+                put(MoodEntryContract.MoodEntry.COLUMN_ANGER, moodEntry.Anger)
+                put(MoodEntryContract.MoodEntry.COLUMN_LOVE, moodEntry.Love)
+                put(MoodEntryContract.MoodEntry.COLUMN_STRESS, moodEntry.Stress)
+                put(MoodEntryContract.MoodEntry.COLUMN_ENERGY, moodEntry.Energy)
+                put(MoodEntryContract.MoodEntry.COLUMN_SLEEP, moodEntry.Sleep)
+                put(MoodEntryContract.MoodEntry.COLUMN_HEALTH, moodEntry.Health)
+                put(MoodEntryContract.MoodEntry.COLUMN_DEPRESSION, moodEntry.Depression)
+                put(MoodEntryContract.MoodEntry.COLUMN_NOTES, moodEntry.Notes)
             }
 
             db.insert(MoodEntryContract.MoodEntry.TABLE_NAME, null, values)
@@ -43,9 +47,13 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
             val projection = arrayOf(
                 MoodEntryContract.MoodEntry.COLUMN_DATE,
                 MoodEntryContract.MoodEntry.COLUMN_HAPPINESS,
+                MoodEntryContract.MoodEntry.COLUMN_ANGER,
                 MoodEntryContract.MoodEntry.COLUMN_LOVE,
+                MoodEntryContract.MoodEntry.COLUMN_STRESS,
                 MoodEntryContract.MoodEntry.COLUMN_ENERGY,
+                MoodEntryContract.MoodEntry.COLUMN_SLEEP,
                 MoodEntryContract.MoodEntry.COLUMN_HEALTH,
+                MoodEntryContract.MoodEntry.COLUMN_DEPRESSION,
                 MoodEntryContract.MoodEntry.COLUMN_NOTES
             )
 
@@ -63,19 +71,27 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
                 while (moveToNext()) {
                     val dateColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_DATE)
                     val happinessColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_HAPPINESS)
+                    val angerColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_ANGER)
                     val loveColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_LOVE)
+                    val stressColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_STRESS)
                     val energyColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_ENERGY)
+                    val sleepColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_SLEEP)
                     val healthColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_HEALTH)
+                    val depressionColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_DEPRESSION)
                     val notesColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_NOTES)
 
                     val date = DateUtilities.getLocalDateFromStringDate(getString(dateColumnIndex))
                     val happiness = getInt(happinessColumnIndex)
+                    val anger = getInt(angerColumnIndex)
                     val love = getInt(loveColumnIndex)
+                    val stress = getInt(stressColumnIndex)
                     val energy = getInt(energyColumnIndex)
+                    val sleep = getInt(sleepColumnIndex)
                     val health = getInt(healthColumnIndex)
+                    val depression = getInt(depressionColumnIndex)
                     val notes = getString(notesColumnIndex) ?: ""
 
-                    val moodEntry = MoodEntry(date, happiness, love, energy, health, notes)
+                    val moodEntry = MoodEntry(date, happiness, anger, love, stress, energy, sleep, health, depression, notes)
                     moodEntryList.add(moodEntry)
                 }
             }
@@ -98,9 +114,13 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
             val projection = arrayOf(
                 MoodEntryContract.MoodEntry.COLUMN_DATE,
                 MoodEntryContract.MoodEntry.COLUMN_HAPPINESS,
+                MoodEntryContract.MoodEntry.COLUMN_ANGER,
                 MoodEntryContract.MoodEntry.COLUMN_LOVE,
+                MoodEntryContract.MoodEntry.COLUMN_STRESS,
                 MoodEntryContract.MoodEntry.COLUMN_ENERGY,
+                MoodEntryContract.MoodEntry.COLUMN_SLEEP,
                 MoodEntryContract.MoodEntry.COLUMN_HEALTH,
+                MoodEntryContract.MoodEntry.COLUMN_DEPRESSION,
                 MoodEntryContract.MoodEntry.COLUMN_NOTES
             )
 
@@ -125,19 +145,27 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
                 while (moveToNext()) {
                     val dateColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_DATE)
                     val happinessColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_HAPPINESS)
+                    val angerColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_ANGER)
                     val loveColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_LOVE)
+                    val stressColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_STRESS)
                     val energyColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_ENERGY)
+                    val sleepColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_SLEEP)
                     val healthColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_HEALTH)
+                    val depressionColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_DEPRESSION)
                     val notesColumnIndex = getColumnIndex(MoodEntryContract.MoodEntry.COLUMN_NOTES)
 
                     val date = DateUtilities.getLocalDateFromStringDate(getString(dateColumnIndex))
                     val happiness = getInt(happinessColumnIndex)
+                    val anger = getInt(angerColumnIndex)
                     val love = getInt(loveColumnIndex)
+                    val stress = getInt(stressColumnIndex)
                     val energy = getInt(energyColumnIndex)
+                    val sleep = getInt(sleepColumnIndex)
                     val health = getInt(healthColumnIndex)
+                    val depression = getInt(depressionColumnIndex)
                     val notes = getString(notesColumnIndex) ?: ""
 
-                    val moodEntry = MoodEntry(date, happiness, love, energy, health, notes)
+                    val moodEntry = MoodEntry(date, happiness, anger, love, stress, energy, sleep, health, depression, notes)
                     moodEntryInRangeList.add(moodEntry)
                 }
             }
@@ -157,9 +185,13 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
         val projection = arrayOf(
             MoodEntryContract.MoodEntry.COLUMN_DATE,
             MoodEntryContract.MoodEntry.COLUMN_HAPPINESS,
+            MoodEntryContract.MoodEntry.COLUMN_ANGER,
             MoodEntryContract.MoodEntry.COLUMN_LOVE,
+            MoodEntryContract.MoodEntry.COLUMN_STRESS,
             MoodEntryContract.MoodEntry.COLUMN_ENERGY,
+            MoodEntryContract.MoodEntry.COLUMN_SLEEP,
             MoodEntryContract.MoodEntry.COLUMN_HEALTH,
+            MoodEntryContract.MoodEntry.COLUMN_DEPRESSION,
             MoodEntryContract.MoodEntry.COLUMN_NOTES
         )
 
@@ -180,17 +212,25 @@ class MoodEntriesRepository @Inject constructor(context: Context) : IMoodEntries
             if (c.moveToFirst()) {
                 val dateIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_DATE)
                 val happinessIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_HAPPINESS)
+                val angerIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_ANGER)
                 val loveIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_LOVE)
+                val stressIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_STRESS)
                 val energyIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_ENERGY)
+                val sleepIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_SLEEP)
                 val healthIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_HEALTH)
+                val depressionIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_DEPRESSION)
                 val notesIndex = c.getColumnIndexOrThrow(MoodEntryContract.MoodEntry.COLUMN_NOTES)
 
                 MoodEntry(
                     date = DateUtilities.getLocalDateFromStringDate(c.getString(dateIndex)),
                     happiness = c.getInt(happinessIndex),
+                    anger = c.getInt(angerIndex),
                     love = c.getInt(loveIndex),
+                    stress = c.getInt(stressIndex),
                     energy = c.getInt(energyIndex),
+                    sleep = c.getInt(sleepIndex),
                     health = c.getInt(healthIndex),
+                    depression = c.getInt(depressionIndex),
                     notes = c.getString(notesIndex)
                 )
             } else {
