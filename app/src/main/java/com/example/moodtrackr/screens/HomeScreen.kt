484af937bc5547a8b5807cbb693367f98b559321
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-09-04 18:31:03 +0200

    Redesign setting button in home screen #70

--- app/src/main/java/com/example/moodtrackr/screens/HomeScreen.kt
+++ app/src/main/java/com/example/moodtrackr/screens/HomeScreen.kt
@@ -1,15 +1,22 @@
 package com.example.moodtrackr.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -25,10 +32,11 @@ fun HomeScreen(
 ) {
     val name = getName(profilePreferencesRepository)
 
-    Column(
+    Row(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = "Hello $name",
@@ -37,16 +45,25 @@ fun HomeScreen(
                 fontSize = 20.sp,
                 textAlign = TextAlign.Center
             ),
-            modifier = Modifier.padding(vertical = 8.dp)
+            modifier = Modifier.weight(1f)
         )
 
         Button(
             onClick = { navController.navigate(Routes.Settings.toString()) },
             modifier = Modifier
-                .fillMaxWidth()
                 .height(48.dp)
+                .background(
+                    color = Color.Transparent,
+                    shape = MaterialTheme.shapes.medium
+                ),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            contentPadding = PaddingValues(0.dp)
         ) {
-            Text("Settings")
+            Icon(
+                imageVector = Icons.Default.Settings,
+                contentDescription = null,
+                tint = Color.Black
+            )
         }
     }
 }
