6ad9ffcdcd58162ff6a6833d1c101ae67e29083b
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-09-04 22:09:38 +0200

    Fixed home screen alignment #70

--- app/src/main/java/com/example/moodtrackr/components/MoodEntrySummary.kt
+++ app/src/main/java/com/example/moodtrackr/components/MoodEntrySummary.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,7 +26,9 @@ fun MoodEntrySummary(
     ) {
         Text(
             text = LocalDate.now().toString(),
-            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp)
+            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -92,9 +95,4 @@ fun MoodEntrySummary(
             )
         }
     }
-}
-
-fun onClick()
-{
-
 }
--- app/src/main/java/com/example/moodtrackr/screens/HomeScreen.kt
+++ app/src/main/java/com/example/moodtrackr/screens/HomeScreen.kt
@@ -1,10 +1,11 @@
 package com.example.moodtrackr.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
@@ -14,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -37,13 +39,35 @@ fun HomeScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(8.dp)
     ) {
-        Text(
-            text = "Hello $name",
-            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp),
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Hello $name",
+                style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp),
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+            )
+
+            Button(
+                onClick = { navController.navigate(Routes.Settings.toString()) },
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "Settings"
+                )
+            }
+        }
 
         Text(
             text = "How do you feel today?",
@@ -55,26 +79,9 @@ fun HomeScreen(
         )
 
         Spacer(modifier = Modifier.weight(1f))
-
-        Button(
-            onClick = { navController.navigate(Routes.Settings.toString()) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(48.dp)
-                .padding(top = 16.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent,
-                contentColor = MaterialTheme.colorScheme.primary
-            )
-        ) {
-            Icon(
-                imageVector = Icons.Default.Settings,
-                contentDescription = "Settings",
-                modifier = Modifier.padding(end = 8.dp)
-            )
-        }
     }
 }
+
 
 
 fun getName(profilePreferencesRepository: IProfilePreferencesRepository): String {
