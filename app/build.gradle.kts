37687b0c8d9ae1b252323c3a7bbbb6b2444d51cc
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-09-11 19:29:52 +0200

    Updated version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.example.moodtrackr"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
+        versionCode = 12
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
