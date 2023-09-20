c07e649cdb0371e3df96254edb517f585961564c
Massimiliano Paolo Pisu <mpp-su@pm.me>
2023-09-20 12:44:50 +0200

    Created CustomFileSystemIO #129

---
+++ app/src/main/java/com/example/moodtrackr/dataAccess/CustomFileSystemIO.kt
@@ -0,0 +1,76 @@
+package com.example.moodtrackr.dataAccess
+
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.example.moodtrackr.dataAccess.interfaces.IFileSystemIO
+import java.io.*
+
+class CustomFileSystemIO : IFileSystemIO {
+
+    private val _defaultPath: String = "Download"
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun write(context: Context, path: String?, fileName: String, data: String): Boolean {
+        try {
+            val file: File
+            val rootDir = Environment.getExternalStorageDirectory()
+
+            if (path != null) {
+                file = File("$rootDir/$path", fileName)
+                if (!file.exists()) {
+                    file.mkdirs()
+                }
+            } else {
+                file = File("$rootDir/$_defaultPath", fileName)
+            }
+
+            val fileOutputStream = FileOutputStream(file)
+            fileOutputStream.write(data.toByteArray())
+            fileOutputStream.close()
+            return true
+        } catch (ex: Exception) {
+            Log.e("CustomFileSystemIO.write", ex.stackTraceToString())
+        }
+
+        return false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun read(context: Context, path: String?, fileName: String): String? {
+        try {
+            val file: File
+            val rootDir = Environment.getExternalStorageDirectory()
+
+            file = if (path != null) {
+                File("$rootDir/$path", fileName)
+            } else {
+                File("$rootDir/$_defaultPath", fileName)
+            }
+
+            if (file.exists()) {
+                val fileInputStream = FileInputStream(file)
+                val inputStreamReader = InputStreamReader(fileInputStream)
+                val bufferedReader = BufferedReader(inputStreamReader)
+
+                val stringBuilder = StringBuilder()
+                var line: String?
+
+                while (bufferedReader.readLine().also { line = it } != null) {
+                    stringBuilder.append(line)
+                }
+
+                fileInputStream.close()
+                return stringBuilder.toString()
+            } else {
+            Log.d("CustomFileSystemIO.read", "File not found: $fileName")
+        }
+        } catch (ex: Exception) {
+            Log.e("CustomFileSystemIO.read", ex.stackTraceToString())
+        }
+
+        return null
+    }
+}
